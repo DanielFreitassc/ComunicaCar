5a7b19eb456aef40dc3c187be02b2acef2f608c6
Geanlucca Zappe <gzubialli@gmail.com>
2025-06-20 20:09:29 -0300

    tela home atendente

--- app/src/navigation/Public/PrivateNavigation.routes.jsx
+++ app/src/navigation/Public/PrivateNavigation.routes.jsx
@@ -1,13 +1,19 @@
+// app/navigation/public/PrivateNavigationRoutes.jsx
+
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../../screens/Mecanico/Home';
+// Import the default export from your Home screen folder:
+import Home from '../../screens/Atendente/Home';
+
+const Stack = createNativeStackNavigator();
 
 export function PrivateNavigationRoutes() {
-    const Stack = createNativeStackNavigator();
-
-    return (
-        <Stack.Navigator initialRouteName='Home'
-            screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
 }
